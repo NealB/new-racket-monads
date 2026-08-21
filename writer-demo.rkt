7917790027d4588850e3b0744a0149141b94bd7d
Neal Burns <nealburns5@gmail.com>
2026-08-21 11:02:39 -0400

    added writer monad

---
+++ writer-demo.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(require racket/control)
+(require "monad-run.rkt")
+(require "writer.rkt")
+
+(define run-writer (make-run Writer.return Writer.bind Writer.format))
+
+
+(define (add5 x)
+  (cons (+ 5 x) (format "Add 5 to ~a" x)))
+
+(define (write-log msg . args)
+  (cons #f (apply format msg args)))
+
+(define results1
+  (run-writer
+   (λ (!)
+     (let* ((a (! Writer.return 34))
+            (b (! add5 a))
+            (c (! add5 b))
+            (d (! add5 19))
+            (e (! add5 2)))
+
+       #f)
+
+     (for ((i 3))
+       (! write-log "final log message ~a" i))
+     )))
+
+(display (~a "results:\n" results1 (~a "done\n")))
+(newline)
---
+++ writer.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(require racket/control)
+
+(define (ensure-list v) (if (list? v) v (list v)))
+
+(define (append-log next-log prev-log)
+  (append (ensure-list next-log) (ensure-list prev-log)))
+  
+(define (Writer.bind prev func)
+  (let* ((pval (car prev))
+         (plog (cdr prev))
+         (next (func pval))
+         (nval (car next))
+         (nlog (cdr next)))
+    (cons nval (append-log nlog plog))))
+         
+(define (Writer.return v) (cons v (list)))
+
+
+(define (Writer.format w)
+  (apply string-append (map (curry format "~a~n") (reverse (cdr w)))))
+
+(provide Writer.bind Writer.return Writer.format)
